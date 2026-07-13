77e8d2932cd8aa45520066ecc2f5db446c932296
sachinp <sachinp@iitbhilai.ac.in>
2026-07-13 22:42:28 +0530

    .

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // console.log("Hey I am JS");
-
+// console.log(a);
+// console.log(b);
 var a = 10;
-var b = 20;
+let b = 20;
+// const c = 30;
+
+console.log(multiply(a, b));
 
 function multiply(x, y) {
+  // let b = 20;
+  // var a = 100;
+  // console.log(a);
   var result = x * y;
   return result;
 }
 
-var answer = multiply(a, b);
+// var answer = multiply(a, b);
 
-console.log("The answer is: " + answer);
+// console.log("The answer is: " + answer);
