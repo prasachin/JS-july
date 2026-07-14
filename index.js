551be5c12dbbf7a175f0e7273aa6387a5b659b6a
sachinp <sachinp@iitbhilai.ac.in>
2026-07-14 22:59:31 +0530

    arrays

--- index.js
+++ index.js
@@ -1,20 +1,95 @@
-// console.log("Hey I am JS");
+// // console.log("Hey I am JS");
+// // console.log(a);
+// // console.log(b);
+// var a = 10;
+// let b = 20;
+// // const c = 30;
+
+// console.log(multiply(a, b));
+
+// function multiply(x, y) {
+//   // let b = 20;
+//   // var a = 100;
+//   // console.log(a);
+//   var result = x * y;
+//   return result;
+// }
+
+// // var answer = multiply(a, b);
+
+// // console.log("The answer is: " + answer);
+
+// let a = {};
+
+// console.log(typeof a);
+
+// let a = 10;
+// let b = a;
+
+// b = 20;
+
+// console.log(a, b);
+
+// let a = {x:10};
+// let b = a;
+
+// b.x = 20;
+
+// console.log(a, b);
+
+// let a = [
+//   1,
+//   2,
+//   3,
+//   4,
+//   5,
+//   "Sachin",
+//   null,
+//   false,
+//   function x() {
+//     console.log("Hey I am a function");
+//   },
+//   { name: "Sachin", age: 30 },
+// ];
+// console.log(a[3]);
+// console.log(a[8]());
+
+// let a = [1, 2, 3, 4, 5];
+// console.log(typeof a);//Object
+// console.log(Array.isArray(a)); //true
+// console.log(a.length);
+
+// function Array(){
+//   this.x = 10;
+// }
+
+// let a = new Array(5);
+
+// console.log(Array.isArray(a));
+// console.log(a.length);
+
+// let a = [1];
 // console.log(a);
-// console.log(b);
-var a = 10;
-let b = 20;
-// const c = 30;
+// // a.push(5, 6); add data at the end of the array
+// console.log(a);
 
-console.log(multiply(a, b));
+// let a = [1];
+// console.log(a);
+// a.unshift(5, 6);
+// console.log(a);
 
-function multiply(x, y) {
-  // let b = 20;
-  // var a = 100;
-  // console.log(a);
-  var result = x * y;
-  return result;
-}
+// let a = [1, 2];
+// console.log(a);
+// a.pop();
+// console.log(a);
 
-// var answer = multiply(a, b);
 
-// console.log("The answer is: " + answer);
+// let a = [1, 2];
+// console.log(a);
+// a.shift();
+// console.log(a);
+
+// let a = [1, 2, 3, 4, 5];
+// a.splice(2, 0, 31, 23);
+// console.log(a);
+
